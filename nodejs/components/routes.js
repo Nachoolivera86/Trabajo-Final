f0b7bba86744406222688166f06c2a2afd319f31
ignacio-olivera <ignacio.olivera@globant.com>
2022-09-25 19:15:47 -0300

    Metodos individuo

---
+++ nodejs/components/routes.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const routes = express.Router()
+const { getIndividuo, getIndividuoById, insertIndividuo,deleteIndividuoById } = require('./operations')
+const { pool } = require('../config/connect')
+
+
+routes.get('/individuo', (req, res) => {
+    getIndividuo(pool, req, result => {
+        res.json(result)
+    })
+})
+
+routes.get('/individuo/:id', (req, res) => {
+    getIndividuoById(pool, req.params.id, result => {
+        res.json(result)
+    })
+})
+
+routes.post('/individuo/insert', (req, res) => {
+
+    let body = JSON.parse(JSON.stringify(req.body));
+    insertIndividuo(pool, body, result => {
+        res.status(201)
+        res.json({
+            message: 'new individuo created'
+        });
+    })
+})
+
+routes.delete('/individuo/:id', (req, res) => {
+    deleteIndividuoById(pool, req.params.id, result => {
+        res.json({
+            message: 'individuo deleted'
+        });
+    })
+})
+
+
+module.exports = routes;
+
