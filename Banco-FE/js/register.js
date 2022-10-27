2bec3bbe18a209f46a5993fc9b0d97b621865cb4
ignacio-olivera <ignacio.olivera@globant.com>
2022-10-27 00:35:40 -0300

    Ajustes Operacion Registrar

--- Banco-FE/js/register.js
+++ Banco-FE/js/register.js
@@ -2,57 +2,99 @@
 let user = "";
 let password = "";
 let checkPass = "";
-let respuestaALlamada;
+let saldo = 0;
+let datosCuentas1 = 0;
+let datosCuentas2 = 0;
+let datosCuentas3 = 0;
+
 
 function registrar(){
-    user = document.getElementById("login-user1").value;
-    password = document.getElementById("login-password1").value;
-    checkPass = document.getElementById("login-password2").value;
+    user = document.getElementById("register-user").value;
+    password = document.getElementById("register-password").value;
+    checkPass = document.getElementById("register-passwordd").value;
     console.log(user);
     console.log(password);
     console.log(checkPass);
 
-
+    if (!user || !password || !checkPass){
+        console.log("Uno de los campos esta vacio")
+        return
+    } 
 
     if(password === checkPass){
         registrarUsuario();
+        registrarCuenta();
     }else{
         console.log("datos invalidos");
     }
 }
 
+let btnRegistrar = document.getElementById("btnContinue");
+btnRegistrar.addEventListener("click",registrar);
 
-
-
-
-
-
-const llamada = async () => {
+const datosUsuario = async () => {
 	try{
-		let resp = await fetch('http://localhost:8080/api/v1/usuario').then(res => res.json())
-		respuestaALlamada = resp;
+		let usuarios = await fetch('http://localhost:8080/api/v1/usuario').then(res => res.json())
+		datosUsuarios = usuarios;
+        console.log(datosUsuarios)
     }catch (error){
 		console.log(error)
 	}
 }
 
 
+const datosCuenta = async () => {
+    try {
+        let cuentas = await fetch('http://localhost:8080/api/v1/cuentas').then(res => res.json())
+        datosCuentas = cuentas;
+        console.log(datosCuentas)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+const maxIdCuenta = async () => {
+    try {
+        let cuentas = await fetch('http://localhost:8080/api/v1/maxIdCu').then(res => res.json())
+        datosCuentas1 = cuentas;
+        console.log(datosCuentas1)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+const maxIdCbu = async () => {
+    try {
+        let cuentas = await fetch('http://localhost:8080/api/v1/maxCbu').then(res => res.json())
+        datosCuentas2 = cuentas;
+        console.log(datosCuentas2)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+const maxIdCLiente = async () => {
+    try {
+        let cuentas = await fetch('http://localhost:8080/api/v1/maxCli').then(res => res.json())
+        datosCuentas3 = cuentas;
+        console.log(datosCuentas3)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
 const registrarUsuario = async () => {
 
-    console.log(user);
-    console.log(password);
-    console.log(checkPass);
-
-
-    let ultimoId = (respuestaALlamada.length) + 1;
-    console.log(ultimoId);
+    let ultimoIdUsuario = (datosUsuarios.length) + 1;
+    console.log(ultimoIdUsuario);
     let data2 = {
-        "Id" : ultimoId,
+        "Id" : ultimoIdUsuario,
         "User": user,
         "Password": password
     }
     try {
-        let response3 = await fetch('http://localhost:8080/api/v1/usuario/insert', {
+        let response3 = await fetch('http://localhost:8080/api/v1/usuario/insert', 
+        {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -66,13 +108,45 @@ const registrarUsuario = async () => {
              console.log("todo mal")
          }
     } catch (error) {
-        console.log("salio or el catch");
+        console.log("salio por el catch");
+    }
+}
+
+const registrarCuenta = async () => {
+    let nuevoIdCuenta = (datosCuentas.length) + 1;
+    let nuevoNroCta = (datosCuentas1[0].maximoNumeroCuenta) + 1;
+    let nuevoCbu = (datosCuentas2[0].maximoNumeroCbu) +1;
+    let clienteId = (datosCuentas3[0].maximoNumeroClienteId) + 1;
+    let data3 = {
+        "id" : nuevoIdCuenta,
+        "nrocta": nuevoNroCta,
+        "cbu": nuevoCbu,
+        "saldo": saldo,
+        "ClienteId": clienteId
+    }
+    try {
+        let response4 = await fetch('http://localhost:8080/api/v1/cuenta/insert', 
+        {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data3)
+        });
+        if (await response4.json()) {
+            console.log("Salio todo bien en cuentas");
+         }
+         else {
+             console.log("todo mal")
+         }
+    } catch (error) {
+        console.log("salio por el catch");
     }
 }
 
 
-let btnRegistrar = document.getElementById("btnContinue");
-btnRegistrar.addEventListener("click",registrarUsuario);
-
-
-llamada();
+datosUsuario();
+datosCuenta();
+maxIdCuenta();
+maxIdCbu();
+maxIdCLiente();
